d272660e41d68e95f72cf265d315d6e85e831ebe
AdonisRD007 <107369933+AdoniRD007@users.noreply.github.com>
2022-06-13 07:42:50 -0400

    Update telegram-bot.py

--- telegram-bot.py
+++ telegram-bot.py
@@ -13,7 +13,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "YOUR OPENAI API KEY GOES HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -21,7 +21,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "YOUR TELEGRAM BOT KEY GOES HERE"
+    tgkey = "5434778620:AAFAX1L7-1C3q9bvCN867K-Um_RAYd0XXMQ"
 else:
     tgkey = tgenv
 print(tgenv)
